449a2d15f2315f29656466bfaa9f9de8114322ae
Julio Cesar Hernandez <jchernandez@aceroprime.com>
2024-11-13 20:57:40 -0600

    listo

---
+++ Ejemplo13112024/01variablesNumericas.js
@@ -0,0 +1,18 @@
+var metros =  parseInt(prompt("Ingresa los metros"));
+//cALCULAR LOS kILOMETROS
+var kilometros=metros/1000;
+//Muestra
+console.log("En Kilometros es " + kilometros);
+
+var Peso = parseInt(prompt("Ingresa tu peso"));;
+var altura = parseInt(prompt("Ingresa tu altura"));;
+ var IMC = Peso/(altura * altura);
+
+console.log("En Resultado IMC " + IMC);
+
+var C   =  parseInt(prompt("Ingresa la temperatura"));;
+
+var F = (9/5 * C) +32
+
+console.log("En Resultado F " + F);
+
